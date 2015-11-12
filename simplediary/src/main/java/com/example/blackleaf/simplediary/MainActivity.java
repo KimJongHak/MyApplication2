36d48ddda23b8d914473b05f39f00d90b57c2b98
Blackleaf <samesay1@naver.com>
2015-11-12 10:34:31 +0900

    Week11

---
+++ simplediary/src/main/java/com/example/blackleaf/simplediary/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.blackleaf.simplediary;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
+    DatePicker dp;
+    EditText edtDiary;
+    Button btnWrite;
+    String fileName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 일기장");
+
+        dp = (DatePicker) findViewById(R.id.datePicker1);
+        edtDiary = (EditText) findViewById(R.id.edtDiary);
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+
+        Calendar cal = Calendar.getInstance();
+        int cYear = cal.get(Calendar.YEAR);
+        int cMonth = cal.get(Calendar.MONTH);
+        int cDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener(){
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                fileName = Integer.toString(year) + "_" + Integer.toString(monthOfYear+1) + "_"
+                        + Integer.toString(dayOfMonth) + ".txt";
+                String str = readDiary(fileName);
+                edtDiary.setText(str);
+                btnWrite.setEnabled(true);
+            }
+        });
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_WORLD_WRITEABLE);
+                    String str = edtDiary.getText().toString();
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(), fileName + " 이 저장됨", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+
+                }
+            }
+        });
+    }
+    String readDiary(String fName) {
+        String diaryStr = null;
+        FileInputStream infs;
+        try {
+            infs = openFileInput(fName);
+            byte[] txt = new byte[500];
+            infs.read(txt);
+            infs.close();
+            diaryStr = (new String(txt)).trim();
+            btnWrite.setText("수정하기");
+        } catch (IOException e) {
+            edtDiary.setHint("일기 없음");
+            btnWrite.setText("새로 저장");
+        }
+
+        return diaryStr;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
